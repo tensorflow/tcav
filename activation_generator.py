084916afc244dd101a295b162a521296a947be5f
Gareth001 <2gareth2@gmail.com>
2019-01-18 15:59:24 +1000

    Fix ImageActivationGenerator to load images in python 3 and remove transparency channels, and warn the user when they are not using enough classes

--- activation_generator.py
+++ activation_generator.py
@@ -121,8 +121,9 @@ class ImageActivationGenerator(ActivationGeneratorBase):
       tf.logging.error('Cannot find file: {}'.format(filename))
       return None
     try:
-      img = np.array(PIL.Image.open(tf.gfile.Open(filename)).resize(
-          shape, PIL.Image.BILINEAR))
+      # ensure image has no transparency channel
+      img = np.array(PIL.Image.open(tf.gfile.Open(filename, 'rb')).convert(
+          'RGB').resize(shape, PIL.Image.BILINEAR))
       # Normalize pixel values to between 0 and 1.
       img = np.float32(img) / 255.0
       if not (len(img.shape) == 3 and img.shape[2] == 3):
@@ -165,13 +166,16 @@ class ImageActivationGenerator(ActivationGeneratorBase):
           lambda filename: self.load_image_from_file(filename, shape),
           filenames[:max_imgs])
       imgs = [img for img in imgs if img is not None]
+      if len(imgs) <= 1:
+        raise ValueError('You must have more than 1 image in each class to run TCAV.')
     else:
       for filename in filenames:
         img = self.load_image_from_file(filename, shape)
         if img is not None:
           imgs.append(img)
-        if len(imgs) >= max_imgs:
+        if len(imgs) <= 1:
+          raise ValueError('You must have more than 1 image in each class to run TCAV.')
+        elif len(imgs) >= max_imgs:
           break
 
     return np.array(imgs)
-
