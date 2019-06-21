c516a0e497993dfb9f507228ac9a81f6539f4930
jvrsgsty <jvrsgsty@users.noreply.github.com>
2019-06-21 14:54:32 -0700

    Logs progress of parallel TCAV run

--- tcav/tcav.py
+++ tcav/tcav.py
@@ -188,12 +188,14 @@ class TCAV(object):
     # for random exp,  a machine with cpu = 30, ram = 300G, disk = 10G and
     # pool worker 50 seems to work.
     tf.logging.info('running %s params' % len(self.params))
+    results = []
     now = time.time()
     if run_parallel:
       pool = multiprocessing.Pool(num_workers)
-      results = pool.map(lambda param: self._run_single_set(param), self.params)
+      for i, res in enumerate(pool.imap(lambda p: self._run_single_set(p), self.params),1):
+        tf.logging.info('Finished running param %s of %s' % (i, len(self.params)))
+        results.append(res)
     else:
-      results = []
       for i, param in enumerate(self.params):
         tf.logging.info('Running param %s of %s' % (i, len(self.params)))
         results.append(self._run_single_set(param))
