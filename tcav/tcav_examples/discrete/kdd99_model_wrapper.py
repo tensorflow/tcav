b06d7b4b0d2e5f98583228d5853dab14a8b20ed1
a-brain-known-as-ralph <raphaelblanes@google.com>
2020-05-14 14:41:34 -0700

    Contents:
    - model wrapper and activation generator for discrete models
    - KerasModelWrapper
    - Example walkthrough of discrete models using the KDD99 dataset

---
+++ tcav/tcav_examples/discrete/kdd99_model_wrapper.py
@@ -0,0 +1,49 @@
+"""Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from tcav.tcav_examples.discrete.kdd99_model import train_and_save_model
+from tcav.model import KerasModelWrapper
+import tensorflow as tf
+
+
+class KDD99KerasModelWrapper(KerasModelWrapper):
+  """ ModelWrapper for the KDD99 dataset from sklearn.
+
+    This uses the KDD99 dataset from sklearn. It is automatically loaded when we
+    try to train models, so no data downloading is required.
+
+    You can read more about it here:
+    https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_kddcup99.html
+
+    Trains a model and saves it to model_path if None exists
+    To see it in action, please check kdd99_discrete_example.ipynb
+    """
+
+  def __init__(self, sess, model_path, labels_path):
+    if not tf.io.gfile.exists(labels_path):
+      raise Exception(
+          "Labels path does not exist. Please provide a labels file.")
+    # Train a model if we have none
+    if not tf.io.gfile.exists(model_path):
+      train_and_save_model(model_path, labels_path)
+
+    # Initialize the wrapper
+    super(KDD99KerasModelWrapper, self).__init__(sess, model_path, labels_path)
+
+    # Using SparseCategoricalCrossEntropy here
+    self.y_input = tf.placeholder(tf.int64, shape=[None])
+    self.loss = self.model.loss_functions[0](self.y_input,
+                                             self.model.outputs[0])
+    self._make_gradient_tensors()
