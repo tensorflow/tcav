fix: relative tcav computation and plot
- the paper states in section 3.6 for relative tcav, construct a positive
  and negative set, e.g., for concepts P_dot, P_stripe, P_mesh this would
  look like, P_stripe (positive) and {P_dot U p_mesh} (negative).
  However, the code was giving sets like, P_stripe (pos), P_dot (neg).
  Now, the complement set is used for relative tcav as described in the
  paper.
- when computing relative tcav, plot function failed because concepts are
  also 'random' concepts and is_random_concept(concept) is always true,
  therefore. Now, plot for relative tcav handles 'random' concepts
  correctly.

tcav/activation_generator.py
@@ -25,12 +25,13 @@ import numpy as np
 import PIL.Image
 import six
 import tensorflow as tf
+from .utils import CONCEPT_SEPARATOR
 
 class ActivationGeneratorInterface(six.with_metaclass(ABCMeta, object)):
   """Interface for an activation generator for a model"""
 
   @abstractmethod
-  def process_and_load_activations(self, bottleneck_names, concepts):
+  def process_and_load_activations(self, bottleneck_names, concepts, relative_tcav=False):
     pass
 
   @abstractmethod
@@ -53,15 +54,15 @@ class ActivationGeneratorBase(ActivationGeneratorInterface):
   def get_examples_for_concept(self, concept):
     pass
 
-  def get_activations_for_concept(self, concept, bottleneck):
-    examples = self.get_examples_for_concept(concept)
+  def get_activations_for_concept(self, concept, bottleneck, relative_tcav=False):
+    examples = self.get_examples_for_concept(concept, relative_tcav)
     return self.get_activations_for_examples(examples, bottleneck)
 
   def get_activations_for_examples(self, examples, bottleneck):
     acts = self.model.run_examples(examples, bottleneck)
     return self.model.reshape_activations(acts).squeeze()
 
-  def process_and_load_activations(self, bottleneck_names, concepts):
+  def process_and_load_activations(self, bottleneck_names, concepts, relative_tcav=False):
     acts = {}
     if self.acts_dir and not tf.io.gfile.exists(self.acts_dir):
       tf.io.gfile.makedirs(self.acts_dir)
@@ -70,8 +71,10 @@ class ActivationGeneratorBase(ActivationGeneratorInterface):
       if concept not in acts:
         acts[concept] = {}
 
+      # convert concept name to filesystem friendly version
+      concept_filename = concept.replace(CONCEPT_SEPARATOR, "-")
       for bottleneck_name in bottleneck_names:
-        acts_path = os.path.join(self.acts_dir, f'acts_{concept}_{bottleneck_name}') if self.acts_dir else None
+        acts_path = os.path.join(self.acts_dir, f'acts_{concept_filename}_{bottleneck_name}') if self.acts_dir else None
 
         if acts_path and tf.io.gfile.exists(acts_path):
           # load activations from file
@@ -80,7 +83,7 @@ class ActivationGeneratorBase(ActivationGeneratorInterface):
             tf.compat.v1.logging.info(f'Loaded {acts_path} shape {acts[concept][bottleneck_name].shape}')
         else:
           # compute and save activations
-          acts[concept][bottleneck_name] = self.get_activations_for_concept(concept, bottleneck_name)
+          acts[concept][bottleneck_name] = self.get_activations_for_concept(concept, bottleneck_name, relative_tcav)
 
           if acts_path:
             tf.compat.v1.logging.info(f'{acts_path} does not exist, Making one...')
@@ -111,11 +114,19 @@ class ImageActivationGenerator(ActivationGeneratorBase):
     super(ImageActivationGenerator, self).__init__(model, acts_dir,
                                                    max_examples)
 
-  def get_examples_for_concept(self, concept):
-    concept_dir = os.path.join(self.source_dir, concept)
-    img_paths = [
-        os.path.join(concept_dir, d) for d in tf.io.gfile.listdir(concept_dir)
-    ]
+  def get_examples_for_concept(self, concept_names, relative_cav=False):
+    if relative_cav:
+      concepts = concept_names.split(CONCEPT_SEPARATOR)
+    else:
+      concepts = [concept_names]
+
+    img_paths = []
+    for concept in concepts:
+      concept_dir = os.path.join(self.source_dir, concept)
+      img_paths.append(
+          [os.path.join(concept_dir, d) for d in tf.io.gfile.listdir(concept_dir)]
+      )
+    img_paths = np.asarray(img_paths).flatten()
     imgs = self.load_images_from_files(
         img_paths, self.max_examples, shape=self.model.get_image_shape()[:2])
     return imgs

tcav/utils.py
@@ -26,6 +26,7 @@ _KEYS = [
 ]
 
 MAX_RANDOM_EXPERIMENTS = 100
+CONCEPT_SEPARATOR = ':'
 
 
 def create_session(timeout=10000, interactive=True):
@@ -56,7 +57,8 @@ def flatten(nested_list):
 def process_what_to_run_expand(pairs_to_test,
                                random_counterpart=None,
                                num_random_exp=100,
-                               random_concepts=None):
+                               random_concepts=None,
+                               relative_tcav=False):
   """Get concept vs. random or random vs. random pairs to run.
 
     Given set of target, list of concept pairs, expand them to include
@@ -95,17 +97,30 @@ def process_what_to_run_expand(pairs_to_test,
           new_pairs_to_test_t.append((target, [concept_set[0], get_random_concept(i)]))
         i += 1
     elif len(concept_set) > 1:
-      new_pairs_to_test_t.append((target, concept_set))
+      if relative_tcav:
+        for concept in concept_set:
+          negative_set = CONCEPT_SEPARATOR.join(np.setdiff1d(concept_set, concept))  # c2:c3:... excluding c_i where i=j
+          new_pairs_to_test_t.append((target, [concept, negative_set]))
+      else:
+        new_pairs_to_test_t.append((target, concept_set))
     else:
       tf.compat.v1.logging.info('PAIR NOT PROCESSED')
     new_pairs_to_test.extend(new_pairs_to_test_t)
 
-  all_concepts = list(set(flatten([cs + [tc] for tc, cs in new_pairs_to_test])))
+  if relative_tcav:
+    all_concepts = []
+    for tc, cs in new_pairs_to_test:
+      all_concepts.append([tc])
+      for c in cs:
+        all_concepts.append(c.split(':'))
+    all_concepts = list(set(flatten(all_concepts)))
+  else:
+    all_concepts = list(set(flatten([cs + [tc] for tc, cs in new_pairs_to_test])))
 
   return all_concepts, new_pairs_to_test
 
 
-def process_what_to_run_concepts(pairs_to_test):
+def process_what_to_run_concepts(pairs_to_test, relative_tcav=False):
   """Process concepts and pairs to test.
 
   Args:
@@ -124,10 +139,15 @@ def process_what_to_run_concepts(pairs_to_test):
   """
 
   pairs_for_sstesting = []
-  # prepare pairs for concpet vs random.
-  for pair in pairs_to_test:
-    for concept in pair[1]:
-      pairs_for_sstesting.append([pair[0], [concept]])
+  if relative_tcav:
+    # keep things as is
+    return pairs_to_test
+  else:
+    # prepare pairs for concpet vs random.
+    for target, concept_set in pairs_to_test:
+      for concept in concept_set:
+        pairs_for_sstesting.append([target, [concept]])
+
   return pairs_for_sstesting
 
 